0ce1ac98727e1056e307f40e77bfa1ba8abff1ef
Michael Baumgarten <michaelbaumgarten@Michaels-Air-4.fritz.box>
2020-12-12 21:40:15 +0100

    tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -4,8 +4,14 @@ def main():
   dice_sum = 0
   for i in range(0, dice_rolls):
     roll = random.randint(1, 6)
-    print(f'You rolled a {roll}')
     dice_sum += roll
+    if roll == 1:
+      print(f'You rolled a {roll}, critical fail!')
+    elif roll == 6:
+      print(f'You rolled a {roll}, critical success!')
+    else:
+      print(f'You rolled a {roll}')
+
   print(f'You scored {dice_sum}')
 
 if __name__== "__main__":
